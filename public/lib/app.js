c7daaadcab7e12f3642728c6b223e33d4df2a713
abhinavKeshri07 <keshri.abhinav07@gmail.com>
2019-07-24 01:03:18 +0530

    Bootstrap working properly

---
+++ public/lib/app.js
@@ -0,0 +1,15 @@
+var myPortfolio = angular.module('myPortfolio', []);
+
+myPortfolio.config(function(){
+
+});
+
+myPortfolio.run(function(){
+
+});
+
+myPortfolio.controller('MyController', ['$scope', function($scope){
+
+
+
+}]);
